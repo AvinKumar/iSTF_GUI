cb0af9e46cc4bbea004bd456bf2d47195b1d839a
avin-kumar-p <avin.kumar-p@hpe.com>
2016-08-25 14:37:09 +0530

    test

--- src/java/com/angular/ngwebdriver/VariableNotInScopeException.java
+++
@@ -1,9 +0,0 @@
-package com.angular.ngwebdriver;
-
-import org.openqa.selenium.WebDriverException;
-
-public class VariableNotInScopeException extends WebDriverException {
-    public VariableNotInScopeException(String msg) {
-        super(msg);
-    }
-}
